04d38ebf09e8fdadc6c9d63f3164578e8aa27120
gabriel <levitarmouse@gmail.com>
2017-04-24 13:42:00 -0300

    Pruebas integración !preAlpha

--- kissfrmw_post_install.sh
+++ kissfrmw_post_install.sh
@@ -2,5 +2,5 @@
 
 mkdir -p config/kissorm;
 cp -rp ./vendor/levitarmouse/kiss_orm/scripts/post_install.sh ./kissorm_post_install.sh;
-chmod 0775 ./config/kissorm/kissorm_post_install.sh;
+chmod 0775 ./kissorm_post_install.sh;
 ./kissorm_post_install.sh;
